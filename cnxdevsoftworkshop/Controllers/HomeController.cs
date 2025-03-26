cea77ced1a1c33cac4ef4c7fcb47b554ae954e7d
sarayut1169 <bungbangjak@gmail.com>
2025-03-26 12:19:02 +0700

    [first commit]

---
+++ cnxdevsoftworkshop/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using cnxdevsoftworkshop.Models;
+
+namespace cnxdevsoftworkshop.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    public IActionResult Index()
+    {
+       
+        
+        
+        return View();
+    }
+    
+
+
+    
+
+    private static List<string> calculationHistory = new List<string>();
+
+    [HttpPost]
+    public IActionResult Index(double x, double y, string operation)
+    {
+        try
+        {
+            double result = 0;
+            string calculationResult = "";
+        
+            switch (operation)
+            {
+                case "+":
+                    result = x + y;
+                    calculationResult = $"{x} + {y} = {result}";
+                    break;
+                case "-":
+                    result = x - y;
+                    calculationResult = $"{x} - {y} = {result}";
+                    break;
+                case "*":
+                    result = x * y;
+                    calculationResult = $"{x} * {y} = {result}";
+                    break;
+                case "/":
+                    if (y != 0)
+                    {
+                        result = x / y;
+                        calculationResult = $"{x} / {y} = {result}";
+                    }
+                    else
+                    {
+                        ViewData["Result"] = "Cannot divide by zero.";
+                        return View();
+                    }
+                    break;
+                default:
+                    ViewData["Result"] = "Invalid operation.";
+                    return View();
+            }
+
+       
+            calculationHistory.Add(calculationResult);
+
+            Console.WriteLine("result : " + result);
+            ViewData["Result"] = result;
+            ViewData["History"] = calculationHistory;
+
+            return View();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+    }
+
+
+    [HttpPost]
+    public IActionResult ClearHistory()
+    {
+      
+        calculationHistory.Clear();
+
+       
+        return RedirectToAction("Index");
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ cnxdevsoftworkshop/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cnxdevsoftworkshop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
